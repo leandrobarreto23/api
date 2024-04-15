723c68ec8aab364a81f711565176cc8c204f9a90
Leandro <166774589+leandrobarreto23@users.noreply.github.com>
2024-04-14 22:33:21 -0300

    add controller

---
+++ src/controllers/usrersController.js
@@ -0,0 +1,12 @@
+class UsersController {
+
+  // index - GET para listar varios registros
+  // show - GET para exibir um registro especifico
+  // create - POST para criar um registro
+  // update - PUT para atualizar um registro
+  // delete - DELETE para remover um registro
+
+  
+}
+
+module.exports = UsersController
