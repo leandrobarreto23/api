3f2e7e42cb1e8354881734e21278e75845ff0f46
Leandro <166774589+leandrobarreto23@users.noreply.github.com>
2024-04-20 22:50:50 -0300

    treating excesses AppError

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
-// pegando a pasta do express do node_modules e despejando na variavel express
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
+
 const express = require("express");
 
 const routes = require("./routes")
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
+
+app.use(( error, request, response, next ) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
