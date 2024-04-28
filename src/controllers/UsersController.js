2c9f03e575b168b04b8fdf54e4ebe5d06835e900
Leandro <166774589+leandrobarreto23@users.noreply.github.com>
2024-04-28 17:15:47 -0300

    atualizar dados de usuario

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,5 @@
 const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
@@ -24,6 +23,39 @@ class UsersController {
     return response.status(201).json();
   }
   
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user) {
+      throw new AppError("Usuário nao encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este email já está em uso.");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?`,
+    [user.name, user.email, new Date, id]
+  );
+
+  return response.json();
+
+  }
+
 }
 
 module.exports = UsersController;
